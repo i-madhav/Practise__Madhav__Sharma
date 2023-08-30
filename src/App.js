21518322f1e8598e9cd16a35fd6a8d969a39c1c8
Madhav <madhav.shar06ma@gmail.com>
2023-08-30 16:40:37 +0530

    ha

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ export default function App(){
   return(
     <div className="app">
       <Logo/>
+      <Form/>
+      <PackingList/>
+      <Stats/>
     </div>
   )
 }
@@ -19,24 +22,35 @@ function Logo(){
 
 function Form(){
   return(
-    <div className="add-form">
+    <form className="add-form">
+      <h3>Add What you want to Add 👀</h3>
+      <select>
+        {Array.from({length:30} , (_,i)=>i+1).map(num => <option value={num} key={num}>
+          {num}
+        </option>)}
+      </select>
 
-    </div>
+      <input type="text" placeholder="Type here bitch"/>
+      <button>Add Bitch</button>
+    </form>
   )
 }
 
 function PackingList(){
   return(
     <div className="list">
-
+      <ul>
+        {initialItems.map(item=><Item itemObj={item} key={item.id}/>)}
+      </ul>
     </div>
   )
 }
 
-function Item(){
+function Item({itemObj}){
   return(
     <li>
-
+      <span>{itemObj.quantity} {itemObj.description} </span>
+      <button>😒</button>
     </li>
   )
 }
