8cd4a24acaf8ae521d3a477d0237a13a3f8b605f
Madhav <madhav.shar06ma@gmail.com>
2023-08-31 17:18:21 +0530

    gh

--- src/App.js
+++ src/App.js
@@ -1,41 +1,63 @@
+const initialItems = [
+  {id:1,description:"Passport",quantity:2,packed:false},
+  {id:2,description:"socks",quantity:12,packed:true},
+  {id:3,description:"Madhav",quantity:2,packed:true}
+]
+
 export default function App(){
   return (
     <div className="app">
-
+      <Logo/>
+      <Form/>
+      <PackingList/>
+      <Stats/>
     </div>
   )
 }
 
 function Logo(){
-  return <h1>👀Madhav Sharma Project👀</h1>
+  return <h1>👀Madhav Project👀</h1>
 }
 
 function Form(){
   return(
-    <form>
+    <form className="add-form">
+      <h3>Add Here My Lord🫅</h3>
+      <select>
+        {Array.from({length:20} , (_,i)=>i+1).map(num=><option value={num} key={num}>
+          {num}
+        </option>)}
+      </select>
 
+      <input type="text" placeholder="Add Here Lord"/>
+      <button>Add Lord🫅</button>
     </form>
   )
 }
 
 function PackingList(){
   return(
-    <div>
-
+    <div className="list">
+      <ul>
+      {initialItems.map(itemdata=> <Item itemObj ={itemdata} key={itemdata.id}/>)}
+      </ul>
     </div>
   )
 }
 
-function Items(){
+function Item({itemObj}){
   return <li>
-
+    <span style={itemObj.packed ? {} :{textDecoration:"line-through"} }>
+      {itemObj.quantity} {itemObj.description}
+    </span>
+    <button>😎</button>
   </li>
 }
 
 function Stats(){
   return(
-    <footer>
-      
+    <footer className="stats">
+      <em>You have X items on your list</em>
     </footer>
   )
 }
