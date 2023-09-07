93d66e7e896518abdde29f18839e67bb76694fca
Madhav <madhav.shar06ma@gmail.com>
2023-09-07 20:21:23 +0530

    d

--- src/App.js
+++ src/App.js
@@ -1,51 +1,113 @@
-import { useState } from "react";
+import { useState } from "react"
 
-const faqs = [
-  {
-    title: "Where are these chairs assembled?",
-    text:
-      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus."
-  },
-  {
-    title: "How long do I have to return my chair?",
-    text:
-      "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus."
-  },
-  {
-    title: "Do you ship to countries outside the EU?",
-    text:
-      "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!"
-  }
-];
+export default function App(){
+const[items , setItems] = useState([])
 
-export default function App() {
-  return (
-    <div>
-      <Accordion data={faqs}/>
-    </div>
-  );
+function handleAddItem(item){
+  setItems(items=>[...items , item])
 }
 
-function Accordion({data}) {
+function handleDeleteItem(id){
+  setItems(items=>items.filter(item=> item.id !== id))
+}
+
+function handleUpdateItem(id){
+  setItems(items=>items.map(item=> item.id === id ?{...item , packed:!item.packed} : item ))
+}
+
+function handleClearList(){
+  
+  setItems([]);
+}
   return(
-    <div className="accordion">
-        {data.map((item , index)=><Item text={item.text} title={item.title} num={index} key={item.title}/>)}
+    <div className="app ">
+<Logo/>
+<Form onAddItem={handleAddItem} />
+<PackingList itemData={items}  onDeleteItem={handleDeleteItem} onUpdateItem={handleUpdateItem} onClearList={handleClearList} />
+<Stats itemsData ={items} />
     </div>
-  ) 
+  )
 }
 
-function Item({num , text , title}){
-    const[isOpen , setIsOpen] = useState(false)
+function Logo(){
+  return <h1>👀Madhav Sharma👀</h1>
+}
 
-    function handleOnClick(){
-        setIsOpen( isOpen => !isOpen)
-    }
-    return(
-        <div className={`item ${isOpen ? "open" : ""}`} onClick={handleOnClick}>
-            <p className="number">{`0${num+1}`}</p>
-            <p className="text">{title} </p>
-            <p className="icon">{isOpen ? `-` : `+`}</p>
-            {isOpen && <div className="content-box">{text}</div>}
-        </div>
-    )
+function Form({onAddItem}){
+const[description , setDescription] = useState("")
+const[quantity , setQuantity] = useState(1)
+
+function handleOnSubmit(e){
+  e.preventDefault();
+  if(!description) return null;
+  const newItem = {description , id:new Date().getSeconds() ,quantity , packed:false}
+  console.log(newItem)
+
+  setDescription("")
+  setQuantity(1)
+
+  onAddItem(newItem)
+}
+
+  return(
+    <form className="add-form" onSubmit={handleOnSubmit}>
+      <h3>Add Stuff Here Bruh😒</h3>
+      <select value={quantity} onChange={(e)=>setQuantity(Number(e.target.value))}>
+        {
+          Array.from({length:10} , (_,i)=>i+1).map(num=><option value={num} key={num}>
+            {num}
+          </option>)
+        }
+      </select>
+      <input type="text" placeholder="Write Here💦 " value={description} onChange={(e)=>setDescription(e.target.value)}/>
+      <button>Add Stuff💥</button>
+    </form>
+  )
+}
+
+function PackingList({itemData , onDeleteItem , onUpdateItem , onClearList}){
+  const[sortBy , setSortBy] = useState("input")
+let sortedItem;
+if(sortBy === "input") sortedItem = itemData;
+if(sortBy === "description") sortedItem = itemData.slice().sort((a,b)=>a.description.localeCompare(b.description));
+if(sortBy === "packed") sortedItem = itemData.slice().sort((a,b)=>Number(a.packed)-Number(b.packed))
+
+
+  return(
+    <div className="list">
+      <ul>
+        {sortedItem.map(item=><Item itemObj={item} key={item.id}  onDeleteItem={onDeleteItem} onUpdateItem={onUpdateItem} />)}
+      </ul>
+
+    <div className="actions">
+    <select value={sortBy} onChange={(e)=>setSortBy(e.target.value)}>
+      <option value="input"> Sort by input order</option>
+      <option value="description">Sort by description</option>
+      <option value="packed">Sort by packed status</option>
+    </select>
+
+    <button onClick={()=>onClearList(itemData)}>Boom💥</button>
+    </div>
+
+    </div>
+  )
+}
+
+function Item({itemObj , onDeleteItem , onUpdateItem}){
+  return(
+    <li>
+      <input type="checkbox" value ={itemObj} onClick={()=>onUpdateItem(itemObj.id)}/>
+      <span  style={itemObj.packed ? {textDecoration:"line-through"} : {} }>{itemObj.quantity} {itemObj.description}</span>
+      <button onClick={()=>onDeleteItem(itemObj.id)}>❌</button>
+    </li>
+  )
+}
+
+function Stats({itemsData}){
+  const numItem = itemsData.length;
+  return(
+    <footer className="stats">
+      <em>You have {numItem} items in your bag.</em>
+    </footer>
+  )
 }
